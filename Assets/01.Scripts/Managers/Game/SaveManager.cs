ef420ec737dbb5c70dfac9033977dc02b5a62796
방승환 <111488685+DanielMondayOff@users.noreply.github.com>
2023-07-17 15:23:01 +0900

    stage fixed

--- Assets/01.Scripts/Managers/Game/SaveManager.cs
+++ Assets/01.Scripts/Managers/Game/SaveManager.cs
@@ -16,10 +16,7 @@ public class SaveManager : MonoBehaviour
         instance = this;
     }
 
-    /// <summary>
-    /// Start is called on the frame when a script is enabled just before
-    /// any of the Update methods is called the first time.
-    /// </summary>
+
     private void Start()
     {
         if (ES3.KeyExists("Money"))
